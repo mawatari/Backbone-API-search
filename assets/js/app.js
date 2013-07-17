8a67cfab573f566476f26b03bd929bfb2ad09e77
Naoto Mawatari <github.mawatari@gmail.com>
2013-07-17 10:18:32 +0900

    #01 PresidentViewをコミット

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+MyApp.App = Backbone.View.extend({
+    el: '#app',
+
+//    tmpl: MyApp.Templates.layout,
+    tmpl: Handlebars.templates.layout,
+
+    initialize: function() {
+        this.$el.html(this.tmpl());
+
+        this.history = new MyApp.Views.History({
+            el: this.$el.find('#history-list')
+        });
+
+        this.searchBar = new MyApp.Views.SearchBar({
+            el: this.$el.find('#header')
+        });
+
+        this.tabs = new MyApp.Views.Tabs({
+            el: this.$el.find('#search-results')
+        });
+
+        this.footer = new MyApp.Views.Footer({
+            el: this.$el.find('#footer')
+        });
+    }
+});
+
+new MyApp.App();
