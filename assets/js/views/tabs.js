6a254ae60b2098d6a8f394eddaa8720563b17f88
Naoto Mawatari <github.mawatari@gmail.com>
2013-07-17 10:17:37 +0900

    #01 MavagerViewをコミット

---
+++ assets/js/views/tabs.js
@@ -0,0 +1,20 @@
+MyApp.Views.Tabs = Backbone.View.extend({
+//    tmpl: MyApp.Templates.tabs,
+    tmpl: Handlebars.templates.tabs,
+
+    initialize: function() {
+        this.$el.html(this.tmpl());
+
+        this.twitters = new MyApp.Views.SearchResults({
+            el: this.$el.find('#twitter-list'),
+//            tmpl: MyApp.Templates.twitter
+            tmpl: Handlebars.templates.twitter
+        });
+
+        this.hotpeppers = new MyApp.Views.SearchResults({
+            el: this.$el.find('#hotpepper-list'),
+//            tmpl: MyApp.Templates.hotpepper
+            tmpl: Handlebars.templates.hotpepper
+        });
+    }
+});
