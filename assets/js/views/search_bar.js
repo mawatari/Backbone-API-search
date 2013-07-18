b78a904be4fb0f5ccf7a5ed0f9954290f797313b
Naoto Mawatari <github.mawatari@gmail.com>
2013-07-18 15:32:56 +0900

    #02 clickイベントにsearchメソッドをハンドリング フォームの値を得て、グローバルイベントのsearchを発火

--- assets/js/views/search_bar.js
+++ assets/js/views/search_bar.js
@@ -5,7 +5,27 @@ MyApp.Views.SearchBar = Backbone.View.extend({
 //    tmpl: MyApp.Templates.search_bar,
     tmpl: Handlebars.templates.search_bar,
 
+    events: {
+        'click #btn-search': 'search'
+    },
+
     initialize: function() {
         this.$el.html(this.tmpl());
+        this.$el.find('#twitter').attr('checked', true);
+    },
+
+    search: function(e) {
+        var $checked = this.$el.find('input[type=radio]:checked'),
+            query    = $('#query').val(),
+            service  = $checked.val(),
+            search   = {};
+
+        e.preventDefault();
+
+        search.query = query;
+        search.service = service;
+
+        // Global Eventを発火
+        MyApp.mediator.trigger('search', search);
     }
 });
