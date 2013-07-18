9c760e91a2536040478ee8f788d18077aa068f41
Naoto Mawatari <github.mawatari@gmail.com>
2013-07-18 16:00:50 +0900

    #02 グローバルのsearchイベントをハンドリングしてaddHistoryメソッドを実行 その中で、Collectionにキーワード等を1件追加

--- assets/js/views/history.js
+++ assets/js/views/history.js
@@ -6,6 +6,16 @@ MyApp.Views.History = Backbone.View.extend({
     tmpl: Handlebars.templates.history,
 
     initialize: function() {
-        this.$el.html(this.tmpl());
+//        _.bindAll(this);
+        _.bindAll.apply(_, [this].concat(_.functions(this)));
+
+        this.searches = this.options.searches;
+
+        MyApp.mediator.on('search', this.addHistory);
+    },
+
+    addHistory: function(search) {
+        search.id = +new Date();
+        this.searches.create(search);
     }
 });
